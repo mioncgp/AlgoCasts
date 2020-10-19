cbda92481dca5e4ca6e7ed83dcf197263a89f54d
Michael <mioncgp@gmail.com>
2020-10-19 22:06:32 +0400

    d

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,35 +6,9 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-// solution #1
-// function reverse(str) {
-//   return str.split("").reverse().join("");
-// }
-
-// solution #2
-// function reverse(str) {
-//   let nStr = "";
-//   for (let i = str.length; i > 0; i--) {
-//     nStr += str[i - 1];
-//   }
-//   return nStr;
-// }
-
-// // solution #3
-// function reverse(str) {
-//   let nStr = "";
-//   for (let char of str) {
-//     nStr = char + nStr;
-//   }
-//   return nStr;
-// }
-
-// solution #4
-function reverse(str) {
-  const r = str.split("").reduce((acc, currentLetter) => {
-    return (currentLetter += acc);
-  }, "");
-  return r;
+function reverseString(word) {
+  const revesedWord = word.split("").reverse().join("");
+  return revesedWord;
 }
 
 module.exports = reverse;
--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,22 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+  for (let row = 0; row < n; row++) {x
+    let stair = "";
+
+    for (let column = 0; column < n; column++) {
+      if (column <= row) {
+        stair += "#";
+      } else {
+        stair += " ";
+      }
+    }
+
+    console.log(`"${stair}"`);
+  }
+}
+
+steps(3);
 
 module.exports = steps;
